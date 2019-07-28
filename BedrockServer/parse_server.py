5672be3e180279570dfb4778d48d679662ae124e
Alan Wakeford <alan.wakeford@gmail.com>
2019-07-28 12:10:52 -0700

    Working log parsing

--- BedrockServer/parse_server.py
+++ BedrockServer/parse_server.py
@@ -1,18 +1,75 @@
 #!/usr/bin/python3
+import re
+from functools import partial
 
+session = {}
+player = {}
 
-#[2019-07-27 15:18:11 INFO] Starting Server
-#[2019-07-27 15:18:11 INFO] Version 1.12.0.28
-#[2019-07-27 15:18:11 INFO] Session ID 568ca736-c53f-4630-82d6-10fbc1bd030e
-#[2019-07-27 15:18:11 INFO] Level Name: Trinity_Server
-#[2019-07-27 15:18:11 INFO] Game mode: 0 Survival
-#[2019-07-27 15:18:11 INFO] Difficulty: 2 NORMAL
-#[2019-07-27 15:18:13 INFO] IPv4 supported, port: 19132
-#[2019-07-27 15:18:13 INFO] IPv4 supported, port: 57775
-#[2019-07-27 15:18:15 INFO] Server started.
-#[2019-07-27 16:05:08 INFO] Player connected: RoboLegoPlayer, xuid: 2535440106087
-#[2019-07-27 17:24:58 INFO] Player disconnected: RoboLegoPlayer, xuid: 2535440106
+# Line parsing handling functions
+def start_session(time):
+   print("Session started at %s" % time)
+   session = {"start" : time}
 
-with open("server.log") as log:
-   for l in log:
-      print(l)
+def connect(time,name,xuid):
+   print("%s connnected at %s" % (name,time))
+   if not xuid in player:
+      player[xuid] = {}
+      player[xuid]["time"] = []
+   player[xuid]["name"] = name
+   player[xuid]["xuid"] = xuid
+   player[xuid]["status"] = "connected"
+   player[xuid]["time"].insert(0,{"on":time})
+
+def disconnect(time,name,xuid):
+   print("%s disconnnected at %s" % (name,time))
+   player[xuid]["xuid"] = xuid
+   player[xuid]["name"] = name
+   player[xuid]["status"] = "disconnected"
+   player[xuid]["time"][0]["off"] = time
+
+def parse_log(logfile = "server.log"):
+    with open(logfile) as log:
+       for l in log:
+           parse_line(l)
+
+def parse_line(l):
+
+    # Dictionary of regex match strings for parsing the log lines,
+    # Indexed by either handling function or session field.
+    match_str = {
+        start_session : re.compile("^\[(.*) .*\] Starting Server"),
+        "version"     : re.compile(".* Version (.*)"),
+        "session_id"  : re.compile(".* Session ID (.*)"),
+        "level"       : re.compile(".* Level Name: (.*)"),
+        "mode"        : re.compile(".* Game mode: (.*)"),
+        "difficulty"  : re.compile(".* Difficulty: (.*)"),
+        "port"        : re.compile(".* IPv4 supported, port:  (.*)"),
+        connect       : re.compile("^\[(.*) .*\] Player connected: (.*), xuid: (.*)"),
+        disconnect    : re.compile("^\[(.*) .*\] Player disconnected: (.*), xuid: (.*)"),
+    }
+
+    match = {}
+    for handler,m in match_str.items():
+        match = m.search(l)
+
+        if match:
+            if callable(handler):
+                handler(*match.groups()) # call handling function with match groups
+            else:
+                field = handler
+                session[field] = match.group(1) # assum only 1 match for session fields
+
+def online():
+    return [player[id] for id in player.keys() if player[id]["status"]=="connected"]
+
+if __name__== "__main__":
+    parse_log()
+
+    print(session)
+
+    for p in player.values():
+      print(p)
+
+    on_player = online()
+    for p in on_player:
+      print("%s is online" % p["name"])
--- BedrockServer/web_server.py
+++ BedrockServer/web_server.py
@@ -1,10 +1,25 @@
 import cherrypy
+import parse_server
 
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return "Trinity Server is up!"
+        return server_status()
 
+    def server_status():
+        parse_server.parse_log();
+        str = "Trinity Server is up!\n"
+        print("\n")
+        for k,v in parse_server.session.items():
+            str += "%s = %s\n" % (k,v)
+        str += "\n"
+
+        on_players = parse_server.online()
+        for p in on_players:
+            str += "%s is online\n" % p["name"]
+        return str
+
+#print(HelloWorld.server_status())
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.server.socket_port = 80
 
